00cd7c4f7b4c82e70ba3401a5d038b7758625afb
ai6ong <infoofficialisong@gmail.com>
2023-06-16 10:06:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A programe that uses the print function
+* Return: 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
