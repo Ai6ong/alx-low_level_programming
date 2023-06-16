47766a6437b9fb4ed5b42420174584a47107da9d
ai6ong <infoofficialisong@gmail.com>
2023-06-16 10:30:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A programe that uses the print function
+* main - A program that uses the print function
 * Return: 0 (success)
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
