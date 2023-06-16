68f6eb1d4ad9648d02c5a32e462a993f56af39ec
ai6ong <infoofficialisong@gmail.com>
2023-06-16 15:10:49 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - a program that that prints a line followed by a new line to the standard error
+ * Return 1 (Successful)
+ */
+int main(void)
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
