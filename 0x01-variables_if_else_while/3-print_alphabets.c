e39759c90abb416ad422931eee36f40968588b16
ai6ong <infoofficialisong@gmail.com>
2023-06-19 13:08:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - print upper and lowercase alphabets
+ *
+ * Return: 0 (Successful)
+ */
+int main()
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+		putchar("\n")
+		return (0);
+}
