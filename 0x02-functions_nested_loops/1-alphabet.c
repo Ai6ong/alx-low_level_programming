c7fe54b5012f23158fc1bd3cd87186d12ab88dc8
ai6ong <infoofficialisong@gmail.com>
2023-06-21 10:21:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print _alphabet in lowercase
+ * Description: print _alphabet in lowercase
+ * Return: 0 (Successful)
+ */
+
+void print_alphabet(void) /* print _alphabet in lowercase */
+{
+	char letter;
+
+	for (letter - 'a'; letter <= 'z' letter++)
+		_putchar("letter");
+
+	_putchar('\n');
+}
