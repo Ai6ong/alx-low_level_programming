bcde8839634fe9c6540c9354293b947d71b875a1
ai6ong <infoofficialisong@gmail.com>
2023-06-21 10:36:27 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * main - print _putchar
  * Return: 0 (Success)
  */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
